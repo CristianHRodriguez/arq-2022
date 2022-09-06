36dd50e28efbc2b245c49b6f6bca554aa8190c1b
cristian <chrodriguez92@ucatolica.edu.co>
2022-09-06 03:37:13 -0500

    correccion

--- dbpostgres.js
+++ dbpostgres.js
@@ -3,7 +3,7 @@ const {Pool} = require('pg')
 
 //genera un pool
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || POSTGRES_URL //"postgres://hxwtxrmwlrjppd:0a8d45cc3bd3a042c21562076e7e8cb1e2221448b95102eac4174fd6cc766b7a@ec2-107-23-76-12.compute-1.amazonaws.com:5432/dd7ph9cc2v59sp"
+    connectionString: process.env.DATABASE_URL || POSTGRES_URL, //"postgres://hxwtxrmwlrjppd:0a8d45cc3bd3a042c21562076e7e8cb1e2221448b95102eac4174fd6cc766b7a@ec2-107-23-76-12.compute-1.amazonaws.com:5432/dd7ph9cc2v59sp"
     ssl: {
         rejectUnauthorized: false //para que no impida la conexion segura
     }
