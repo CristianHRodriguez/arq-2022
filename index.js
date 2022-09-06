828fcf3b98d9fb76829d00fe762cec961f5792f7
cristian <chrodriguez92@ucatolica.edu.co>
2022-09-06 03:35:34 -0500

    base con bd

--- index.js
+++ index.js
@@ -2,9 +2,19 @@
 const express = require('express')
 //giardar ejecucion de express
 const app = express()
+//llama el pool del modulo
+const {pool} = require('./pg')
 
-//obtener info del sistema operativo -> variables de entorno -> PUERTO
+//->obtener info del sistema operativo -> variables de entorno -> PUERTO
 console.log(process.env.PORT)
+//->enviar a pantalla un mensaje
+app.get('/', (req,res) => res.send("Hello word"))
+//->para trabajar paginas estaticas
+//app.use(express.static('public')) //donde public es la carpeta que contiene los archivos web
+
+//uso base de datos -> crear ruta -> hacer ping
+app.get('ping')
+
 //escuchar del puerto #
-app.listen(process.env.PORT || 3000)
-console.log('Server on port',process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000) //
+console.log('Server on port', process.env.PORT ||  3000) //process.env.PORT || 
